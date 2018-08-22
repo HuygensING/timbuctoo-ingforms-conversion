c244bcec1802b1488bf1674ba356c80d796296de
meindertk <meindert.kroese@huygens.knaw.nl>
2018-08-22 16:28:50 +0200

    use the information from inventariseer_tags.rb to build a parser

---
+++ ingforms/scripts/build_parser.rb
@@ -0,0 +1,164 @@
+require 'open-uri'
+require 'rubygems'
+require 'cgi'
+#require 'htmlentities'
+
+
+def make_start_end item,output,property=""
+    prop = ""
+    seq = ""
+    if property.eql?("literal")
+	prop = ' rdf:parseType=\"Literal\"'
+    elsif property.eql?("B")
+	prop = ' rdf:parseType=\"Resource\"'
+    elsif property.eql?("S")
+	seq = "rdf:Seq"
+    end
+    start =<<EOF
+    def #{item}_start attrs
+        put_out "\\n#\{@indent}<#\{@afk}:#{item}#{prop}>"
+EOF
+    if !seq.empty?
+        start +=<<EOF
+  	put_out "\\n#\{@indent}<rdf:Seq>"
+EOF
+    end
+    start +=<<EOF
+    end
+
+EOF
+    output.puts start
+
+    stop =<<EOF
+    def #{item}_end
+EOF
+    if !seq.empty?
+	stop +=<<EOF
+  	@output.puts "\\n#\{@indent}</rdf:Seq>"
+        indent = "  " * (@level - 1)
+  	put_out "#\{indent}"
+EOF
+    end
+    stop +=<<EOF
+        put_out "</#\{@afk}:#{item}>"
+    end
+
+EOF
+    output.puts stop
+end
+
+def help_message
+    STDERR.puts "use: ruby build_parser -i input -o output"
+    exit(0)
+end
+
+if __FILE__ == $0
+
+    STDERR.puts ""
+    STDERR.puts ""
+    STDERR.puts ""
+    
+    inputfilename = ""
+    outputfilename = ""
+
+    (0..(ARGV.size-1)).each do |i|
+	case ARGV[i]
+	    when '-i' then begin inputfilename = ARGV[i+1] end
+	    when '-o' then begin outputfilename = ARGV[i+1] end
+	   # when '-r' then begin resource = ARGV[i+1] end
+	    when '-h' then begin help_message end
+	end
+    end
+
+    if inputfilename.empty? || outputfilename.empty?
+	help_message
+    end
+
+    output = File.new(outputfilename,"w")
+    #read header
+    File.open("parser_header.txt") do |file|
+	while line = file.gets
+	    line.force_encoding(Encoding::UTF_8)
+	    output.puts line
+	end
+    end
+    #
+    #close initialize
+    output.puts "    end"
+    output.puts
+
+    tags = Array.new
+    table = Hash.new
+    File.open(inputfilename) do |file|
+	while line = file.gets
+	    line.force_encoding(Encoding::UTF_8)
+	    #line.strip!
+	    if !line.empty?
+		# STDERR.puts line.strip
+		md = line.match(/<([^>]*)>/)
+		if !md.nil?
+		    tag = md[1]
+		    if md[1].split("|").size>1
+			old,new = md[1].split("|")
+			tag = new
+			level = (md.pre_match.size / 2 ) + 1
+			if table.has_key?(old) 
+			    table[old][level] = new
+			else
+			    table[old] = {}
+			    table[old][level] = new
+			end
+			STDERR.puts "md.pre_match.size: #{md.pre_match.size}"
+			STDERR.puts "old: #{old} - new: #{new} - level: #{level}"
+		    end
+		    if tags.include? tag
+		        STDERR.puts "multiple #{tag}"
+		    else
+			md_2 = line.match(/\(([^()]*)\)$/)
+			property = ""
+			property = md_2[1] if !md_2.nil?
+		        make_start_end tag,output,property
+			tags << tag
+		    end
+		end
+	    end
+	end
+    end
+
+    table.each do |k,v|
+	puts "k: #{k}"
+	puts "v: #{v}"
+	v.each do |a,b|
+	    puts "a: #{a} - b: #{b}"
+	end
+    end
+    puts table
+ 
+    table_def =<<EOF
+    def table
+       	#{table}
+    end
+EOF
+    output.puts table_def
+    table_def =<<EOF
+    def table
+       	{
+	    "thema" => {2 => "themas"},
+	    "trefwoord" => {2 => "trefwoorden",
+    		4 => "woord" },
+	    "persoon" => {2 => "personen" },
+	    "instelling" => {2 => "instellingen"},
+	}
+    end
+EOF
+
+    # read footer
+    File.open("parser_footer.txt") do |file|
+	while line = file.gets
+	    line.force_encoding(Encoding::UTF_8)
+	    output.puts line
+	end
+    end
+
+end
+
